a495c3e2b8fa205262f6abe4c731b7d75ba95ec6
linoy hadad <linoy.hadad@post.runi.ac.il>
2024-11-29 20:03:08 +0200

    finished

--- Primes.java
+++ Primes.java
@@ -1,5 +1,43 @@
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
+        int N = Integer.parseInt(args[0]);
+        int p = 2;
+        boolean isprime [] = new boolean[N+1];
+        for (int i = 0; i < isprime.length; i++) {
+            if ( i > 1) {
+                isprime[i]= true;
+            }
+            else {
+                isprime[i] = false;
+            }
+        }
+        while (p < Math.sqrt(N)) {
+                int index = p;
+                while (index < isprime.length) {
+                    if(index % p == 0 && index!=p) {
+                        isprime [index] = false;
+                    }
+                    index++;
+                }
+                for (int i = p + 1; i < isprime.length; i++) {
+                    if (isprime[i]) {
+                        p = i;
+                        break;
+                    }
+                }
+        }
+        
+        System.out.println("Prime numbers up to "+N+":");
+        int count = 0;
+            for (int i=0; i < isprime.length; i++) {
+                if (isprime[i]) {
+                    System.out.println(i);
+                    count++;
+                }
+            }
+
+            System.out.println("There are " +count + " primes between 2 and "+N+" ("+(count/30)*100+"% are primes)");
+
+
     }
 }
